39b670b5bca62ea3a33a92182d7f457a6c67bff4
momanio <o.a.momani@gmail.com>
2024-11-27 05:31:37 +0300

    next.config edited

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: false,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
